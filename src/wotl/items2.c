9a67efee5101e32900278b8ae41dc15cd78d7877
Nax <max@bacoux.com>
2023-08-23 04:17:53 +0200

    Add IsItemOnion

--- src/wotl/items2.c
+++ src/wotl/items2.c
@@ -1,4 +1,21 @@
 #include <wotl/items.h>
+#include <wotl/util.h>
+
+int IsItemOnion(u16 itemId)
+{
+    if (itemId == 0x136)
+    {
+        return 1;
+    }
+    else if (itemId == 0x12d || itemId == 0x125 || itemId == 0x120 || itemId == 0x106)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
 int IsItemMinerva(u16 itemId)
 {
