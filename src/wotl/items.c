d9c766f475833b3965970353a907bdd06e5e8297
Nax <max@bacoux.com>
2023-08-23 04:53:19 +0200

    Add IsItemNull, add IsItemRandomPlaceholder

--- src/wotl/items.c
+++ src/wotl/items.c
@@ -1,4 +1,5 @@
 #include <wotl/items.h>
+#include <wotl/util.h>
 
 int IsItemInvalid(u16 itemId)
 {
@@ -18,4 +19,65 @@ int IsItemInvalid(u16 itemId)
     }
 
     return invalid;
+}
+
+int IsItemNull(u16 itemId)
+{
+    if_unlikely (itemId == 0)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int IsItemRandomPlaceholder(u16 itemId)
+{
+    if_unlikely (itemId == 0xfe)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int IsItemElementalGun(u16 itemId)
+{
+    if (itemId == 0x4c)
+    {
+        return 1;
+    }
+    else if (itemId == 0x4b || itemId == 0x4a)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int IsItemOnion(u16 itemId)
+{
+    if (itemId == 0x136)
+    {
+        return 1;
+    }
+    else if (itemId == 0x12d || itemId == 0x125 || itemId == 0x120 || itemId == 0x106)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int IsItemMinerva(u16 itemId)
+{
+    switch (itemId)
+    {
+    case 0x12e:
+        return 1;
+    default:
+        return 0;
+    }
 }
